c6ab1817caef0970492afc5cfbfba8f83aab9116
chaots00 <ludovicmaster@hotmail.com>
2020-09-29 18:31:37 +0200

    librarynew

--- bases/js/main.js
+++ bases/js/main.js
@@ -1,3 +1,15 @@
 
-console.log('mon message');
-console.error('mon message erreur');
+//console.log('mon message');
+//console.error('mon message erreur');
+/*document.addEventListener('click',function(){
+   alert("message");
+});
+*/
+const myButton = document.getElementById('bouton-bleu');
+if(myButton != null){
+myButton.addEventListener ('click',function()
+{
+    const table = document.querySelector('table');
+    table.style.display ="none"; 
+});
+}
