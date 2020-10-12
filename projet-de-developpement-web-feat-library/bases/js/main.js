b43a26018bc310e8a56a807cadbb78e309d5d18e
chaots00 <ludovicmaster@hotmail.com>
2020-10-12 12:00:05 +0200

    car debut

--- projet-de-developpement-web-feat-library/bases/js/main.js
+++ projet-de-developpement-web-feat-library/bases/js/main.js
@@ -4,22 +4,37 @@
 //    alert('MESSAGE');
 //});
 
-/*const myButton = document.getElementById('bouton-bleu');
-if(myButton != null) {
+const myButton = document.getElementById('bouton-bleu');
+
     myButton.addEventListener('click', function(){
        const table = document.querySelector('table');
        table.style.display = 'none';
     });
+    else 
+    {
+
+        table.style.display = 'block';
+    }
+     };
 }
-*/
+
+
+const mysupbt = document.getElementById('bouton-rouge');
+    mysupbt.addEventListener('click', function(){
+       const table = document.getElementsByTagName('tr').length;
+       for i=1;i<taille;i++{
+           document.querySelectorAll('tr')[i].lastElementChild.innerHTML = '';
+       }
+    });
+
 /*const btns = document.getElementsByClassName('btn');
 
 btns[0].addEventListener('click', function(){
     alert('MESSAGE');
 });
 */
-$('#bouton-bleu').on('click',function()
+/*$('#bouton-bleu').on('click',function()
 {
     $('table').find('tr').last().remove();
 });
-
+*/
---
+++ projet-de-developpement-web-feat-library/car/main.js
@@ -0,0 +1,5 @@
+setInterval(function() 
+{
+    document.getElementsByTagName ("car")
+    
+})
